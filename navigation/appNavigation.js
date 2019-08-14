aa41274b5b66e99d7d046f36c2571311e3234ef9
LGA720/胡文寶 <VNW0014639@vn.fpg.com>
2019-08-14 11:16:22 +0700

    navigation

---
+++ navigation/appNavigation.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class AppNavigation extends Component {
+	constructor(props) {
+		super(props);
+		this.initSate();
+	}
+
+	initSate = () => {
+
+	}
+
+	render() {
+		return (
+
+		);
+	}
+}
+
+export default AppNavigation;
